0e598f80013febca04c988a3f607dc0bacf212ed
reject <reject@pop-os.localdomain>
2020-09-29 13:01:56 +0300

    mergeandrecommit

--- views/Home.js
+++ views/Home.js
@@ -1,11 +1,8 @@
-import React from "react";
-import List from "../components/List";
-import PropTypes from "prop-types";
+import React from 'react';
+import List from '../components/List';
+import PropTypes from 'prop-types';
 
-
-import material from "../theme/variables/material";
-
-const Home = ({ navigation }) => {
+const Home = ({navigation}) => {
   // const {navigation} = props;
   // const navigation = props.navigation;
   return <List navigation={navigation} all />;
